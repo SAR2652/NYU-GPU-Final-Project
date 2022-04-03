a4785d1194de5f2d04a98e0629dbd58cce22de36
Sarvesh Relekar <sr5796@nyu.edu>
2022-04-03 16:38:31 -0400

    Created GPU Profiler Class

--- device_info.py
+++ device_info.py
@@ -1,27 +1,41 @@
 import subprocess
 import json
 
-result = subprocess.run('./getDeviceProp.sh', stdout = subprocess.PIPE)
+class GPUProfiler:
+    def __init__(self):
+        result = subprocess.run('./getDeviceProp.sh', stdout = subprocess.PIPE)
+        self.content = result.stdout.decode('utf-8')
+        self.data = None
+    
+    def getDevicePropDict(self):
+        if self.data != None:
+            return self.data
+        lines = self.content.splitlines()
+        self.data = dict()
+        current_device = None
+        for line in lines:
+            key, value = line.split('=')
+            if key == 'Device ID':
+                current_device = 'device:{}'.format(value)
+                self.data[current_device] = dict()
+            else:
+                if any(c.isalpha() for c in value):
+                    self.data[current_device][key] = value
+                else:
+                    self.data[current_device][key] = float(value)
 
-content = result.stdout.decode('utf-8')
+        return self.data
 
-lines = content.splitlines()
-data = dict()
+    def getDevicePropJSON(self):
+        self.data = self.getDevicePropDict()
+        return json.dumps(self.data, indent = 4)
 
-current_device = None
+    def saveAsJSON(self, filepath = 'gpu_details.json'):
+        json_object = self.getDevicePropJSON()
+        if filepath[-5:] != '.json':
+            print('Incorrect File Extension!')
+            return
+        with open(filepath, 'w') as f:
+            json.dump(json_object, f)
 
-for line in lines:
-    key, value = line.split('=')
-    if key == 'Device ID':
-        current_device = 'device:{}'.format(value)
-        data[current_device] = dict()
-    else:
-        if any(c.isalpha() for c in value):
-            data[current_device][key] = value
-        else:
-            data[current_device][key] = float(value)
-
-json_object = json.dumps(data, indent = 4)
-
-with open('gpu_details.json', 'w') as f:
-    json.dump(json_object, f)
+        print('JSON File saved as {}.'.format(filepath))
