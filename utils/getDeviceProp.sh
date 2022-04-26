8b03a3d763744155ce537f64ef2c5e6eadd76ee2
Sarvesh Relekar <sr5796@nyu.edu>
2022-04-25 20:33:39 -0400

    updated deviceprop

--- utils/getDeviceProp.sh
+++ utils/getDeviceProp.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-nvcc -o deviceQuery deviceQuery.cu
+FILE=deviceQuery.cu
+if test -f "$FILE"; then
+    nvcc -o deviceQuery deviceQuery.cu
+else
+    nvcc -o deviceQuery ./utils/deviceQuery.cu
+fi
 ./deviceQuery
+rm deviceQuery
