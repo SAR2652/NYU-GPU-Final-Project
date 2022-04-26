f99b78480f06d543da40b1536ea9f075ee9ab8a0
Sarvesh Relekar <sr5796@nyu.edu>
2022-04-25 21:22:01 -0400

    Final Inference with Scaling

--- inference.py
+++ inference.py
@@ -88,6 +88,6 @@ rf_model = pickle.load(open('random_forest_model.pickle', 'rb'))
 
 dp = sc.transform(np.array(data_point).reshape(1, -1))
 
-prediction = rf_model.predict([list(dp)])
+prediction = rf_model.predict(dp)
 
 print('Approximate Speedup Ratio w.r.t a single thread will be {}'.format(prediction[0]))
